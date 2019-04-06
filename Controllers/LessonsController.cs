4c6b29b469934d13f1b949a96d4363f9340f7f3c
soniasych <sychsofia@gmail.com>
2019-04-06 20:55:55 +0300

    Controllers Created

---
+++ Controllers/LessonsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using danceCoolServer.Models;
+
+namespace danceCoolServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LessonsController : ControllerBase
+    {
+        private readonly DanceCoolContext _context;
+
+        public LessonsController(DanceCoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Lessons
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Lesson>>> GetLesson()
+        {
+            return await _context.Lesson.ToListAsync();
+        }
+
+        // GET: api/Lessons/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lesson>> GetLesson(int id)
+        {
+            var lesson = await _context.Lesson.FindAsync(id);
+
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return lesson;
+        }
+
+        // PUT: api/Lessons/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLesson(int id, Lesson lesson)
+        {
+            if (id != lesson.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lesson).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LessonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Lessons
+        [HttpPost]
+        public async Task<ActionResult<Lesson>> PostLesson(Lesson lesson)
+        {
+            _context.Lesson.Add(lesson);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LessonExists(lesson.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetLesson", new { id = lesson.Id }, lesson);
+        }
+
+        // DELETE: api/Lessons/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Lesson>> DeleteLesson(int id)
+        {
+            var lesson = await _context.Lesson.FindAsync(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            _context.Lesson.Remove(lesson);
+            await _context.SaveChangesAsync();
+
+            return lesson;
+        }
+
+        private bool LessonExists(int id)
+        {
+            return _context.Lesson.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/PaymentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using danceCoolServer.Models;
+
+namespace danceCoolServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly DanceCoolContext _context;
+
+        public PaymentsController(DanceCoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Payments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Payment>>> GetPayment()
+        {
+            return await _context.Payment.ToListAsync();
+        }
+
+        // GET: api/Payments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Payment>> GetPayment(int id)
+        {
+            var payment = await _context.Payment.FindAsync(id);
+
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return payment;
+        }
+
+        // PUT: api/Payments/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPayment(int id, Payment payment)
+        {
+            if (id != payment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(payment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Payments
+        [HttpPost]
+        public async Task<ActionResult<Payment>> PostPayment(Payment payment)
+        {
+            _context.Payment.Add(payment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PaymentExists(payment.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPayment", new { id = payment.Id }, payment);
+        }
+
+        // DELETE: api/Payments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Payment>> DeletePayment(int id)
+        {
+            var payment = await _context.Payment.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Payment.Remove(payment);
+            await _context.SaveChangesAsync();
+
+            return payment;
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payment.Any(e => e.Id == id);
+        }
+    }
+}
