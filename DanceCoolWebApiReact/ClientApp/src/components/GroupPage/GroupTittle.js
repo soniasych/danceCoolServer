395e2606c1cebfc38e611d99140246a126258183
soniasych <sychsofia@gmail.com>
2019-05-20 21:35:05 +0300

    Fixed group maping

--- DanceCoolWebApiReact/ClientApp/src/components/GroupPage/GroupTittle.js
+++ DanceCoolWebApiReact/ClientApp/src/components/GroupPage/GroupTittle.js
@@ -6,6 +6,9 @@ const GroupTittle = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{props.group.groupDirection}</h5>
                 <p className="card-text">{props.group.groupLevel}</p>
+                <p>Наставники</p>
+                <p className="card-text">{props.group.primaryMentor}</p>
+                <p className="card-text">{props.group.secondaryMentor}</p>
             </div>
         </div>
     );
